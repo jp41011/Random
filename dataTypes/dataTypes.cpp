7ba29c89e009c8a927bf6d3247af9e80567a454b
Juan <jp41011@gmail.com>
2014-05-22 01:27:17 -0700

    sizeof enum, struct, empty struct

--- dataTypes/dataTypes.cpp
+++ dataTypes/dataTypes.cpp
@@ -6,6 +6,19 @@ using std::cout;
 using std::endl;
 using std::string;
 
+enum myType{EX, OH};
+
+struct myStruct
+{
+	int id;
+	int age;
+	float weight;
+};
+
+struct myEmpty
+{
+};
+
 int main()
 {
 	// Testing typeid and looking at size of variables
@@ -17,11 +30,15 @@ int main()
 	cout << "Long: " << typeid(long).name() << "\t\tsize: " << sizeof(long) << endl;
 	cout << "Float: " << typeid(float).name() << "\tsize: " << sizeof(float) << endl;
 	cout << "Double: " << typeid(double).name() << "\tsize: " << sizeof(double) << endl;
-	cout << "Pointers" << endl;
+	cout << "---- Pointers ----" << endl;
 	cout << "Char*: " << typeid(char*).name() << "\t\tsize: " << sizeof(char*) << endl;
-	cout << "String*: " << typeid(string*).name() << "\tsize: " << sizeof(string*) << endl;
+	cout << "String*: " << typeid(string*).name() << "\t\tsize: " << sizeof(string*) << endl;
 	cout << "Int*: " << typeid(int*).name() << "\t\tsize: " << sizeof(int*) << endl;
-	cout << "float*: " << typeid(float*).name() << "\tsize: " << sizeof(float*) << endl;
+	cout << "float*: " << typeid(float*).name() << "\t\tsize: " << sizeof(float*) << endl;
+	cout << "---- My Types ----" << endl;
+	cout << "myType: " << typeid(myType).name() << "\t\tsize: " << sizeof(myType) << endl;
+	cout << "myStruct: " << typeid(myStruct).name() << "\tsize: " << sizeof(myStruct) << endl;
+	cout << "myEmpty: " << typeid(myEmpty).name() << "\tsize: " << sizeof(myEmpty) << endl;
 	// size of pointers = 4 bytes (on 32-bit machine)
 	// 					  8 bytes (on 64-bit machine)
 	return 0;
